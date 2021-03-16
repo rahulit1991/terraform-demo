ac8f1e8e91ae564be49360c88eba42c9436a27e8
rahulit1991 <64314085+rahulit1991@users.noreply.github.com>
2021-03-16 08:57:09 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,15 +6,6 @@ variable "AWS_REGION" {
 variable "AWS_ACCESS_KEY" {}
 variable "AWS_SECRET_KEY" {}
 
-variable "INSTANCE_AMI" {
-  /* getting latest image from http://cloud-images.ubuntu.com/locator/ec2/ */
-      type = map(string)
-      default = {
-        us-east-1 = "ami-042e8287309f5df03"
-        ap-south-1 = "ami-0d758c1134823146a"
-        eu-west-1 = "ami-0db61e5fa6d1a815a"
-            }
-}
 variable "key_path" {}
 variable "vpc-cidr-block" {}
 
